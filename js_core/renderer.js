960d1b9265154dc11272ea517208a5f64225a237
Nico_Workstation <nicolaspoin@gmail.com>
2021-05-16 19:54:54 +0200

    Revert "Test scaling 1"
    This reverts commit 64e9d651

--- js_core/renderer.js
+++ js_core/renderer.js
@@ -1,17 +1,5 @@
 class Renderer {
     constructor(canvas) {
-        let height = screen.availHeight;
-        let width = screen.availWidth;
-        let scaleh = 1.;
-        if(height < canvas.height){
-            scaleh = height / canvas.height;
-        }
-        let scalew = 1.;
-        if(width < canvas.width){
-            scalew = width / canvas.width;
-        }
-        let scale = Math.min(scalew, scaleh);
-        
         this._gl = canvas.getContext('webgl2', {
             antialias: false,
             depth: false,
@@ -19,7 +7,7 @@ class Renderer {
             stencil: false
         });
         this._gl.clearColor(0.5, 0.9, 0.5, 0.9);
-        this._gl.viewport(0, 0, canvas.width * scale, canvas.height * scale);
+        this._gl.viewport(0, 0, canvas.width, canvas.height);
         this.clear();
     }
 
