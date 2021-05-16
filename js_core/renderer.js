7889ff53a14baa2e41a7609e175daa4e70e6591f
Nico_Workstation <nicolaspoin@gmail.com>
2021-05-16 19:54:50 +0200

    Revert "Update renderer.js"
    This reverts commit afd31d2d

--- js_core/renderer.js
+++ js_core/renderer.js
@@ -1,16 +1,7 @@
 class Renderer {
     constructor(canvas) {
-        this._canvas = canvas;
-        this._viewPortPixelSize = viewPortPixelSize;
-        let clWidth = canvas.parentNode.clientWidth;
-        let clHeight = canvas.parentNode.clientHeight;
-        let scWidth = screen.availWidth;
-        let scHeight = screen.availHeight;
-        let height = Math.min(clHeight, scHeight);
-        let width = Math.min(clWidth, scWidth);
-        console.log(width);
-        console.log(height);
-
+        let height = screen.availHeight;
+        let width = screen.availWidth;
         let scaleh = 1.;
         if(height < canvas.height){
             scaleh = height / canvas.height;
@@ -20,24 +11,15 @@ class Renderer {
             scalew = width / canvas.width;
         }
         let scale = Math.min(scalew, scaleh);
-
-        console.log(scale);
+        
         this._gl = canvas.getContext('webgl2', {
             antialias: false,
             depth: false,
-            premultipliedAlpha: false,
+            alpha: false,
             stencil: false
         });
-
-        //enable blending
-        this._gl.enable(this._gl.BLEND);
-        this._gl.blendFunc(this._gl.SRC_ALPHA, this._gl.ONE_MINUS_SRC_ALPHA);
-        this._gl.clearColor(1, 1, 1, 1);
-        canvas.width *= scale;
-        canvas.height *= scale;
-        this._gl.viewport(0, 0, canvas.width, canvas.height);
-        // this._gl.viewport(0, 0, canvas.width/scale, canvas.height/scale);
-        //this._gl.viewport(0, 0, canvas.width * scale, canvas.height * scale);
+        this._gl.clearColor(0.5, 0.9, 0.5, 0.9);
+        this._gl.viewport(0, 0, canvas.width * scale, canvas.height * scale);
         this.clear();
     }
 
