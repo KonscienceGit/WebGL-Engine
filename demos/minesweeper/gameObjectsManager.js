c4eea7581448d561e24ab4c2bc76069d530394c6
Nicolas POINSIGNON <nicolas.poinsignon@geotoolkit.net>
2022-05-28 11:34:19 +0200

    Add basic minesweeper tiles

--- demos/minesweeper/gameObjectsManager.js
+++ demos/minesweeper/gameObjectsManager.js
@@ -3,9 +3,9 @@ class GameObjectsManager {
      * @param {Renderer} renderer
      */
     constructor(renderer) {
-        const path = "../../resources/"
+        const path = "../../resources/minesweeper/"
         this.pixelPerfectTool = new PositionTool(renderer);
-        this.tiles = new MultiSprite(renderer, path + "life.png");
+        this.tiles = new MultiSprite(renderer, path + "Tile.png");
 
         //---- Setup the sprite render order from back to front: ----//
         this.spriteArray = [];
